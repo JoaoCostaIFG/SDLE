c921c0fd9685400dda61e7a367272aa95f71c361
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-11-10 18:56:19 +0000

    xsub e xpub

---
+++ src/broker/client.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	zmq "github.com/pebbe/zmq4"
+	"strings"
+)
+
+func main() {
+	zctx, _ := zmq.NewContext()
+
+	requester, _ := zctx.NewSocket(zmq.REQ)
+	defer requester.Close()
+	err := requester.Connect("tcp://localhost:5559")
+	if err != nil {
+		fmt.Print("very sad no connect")
+		return
+	}
+
+	for i := 0; i < 10; i++ {
+		requester.SendMessage("Hello")
+		reply, _ := requester.RecvMessage(0)
+		fmt.Printf("Received reply %d [%s]\n", i, strings.Join(reply, " "))
+	}
+}
