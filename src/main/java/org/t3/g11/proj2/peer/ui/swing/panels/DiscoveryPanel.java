9f3f8108fed1ee5604817c3075e5d24f6829c089
Tiago Silva <tiagodusilva@gmail.com>
2022-01-11 17:06:39 +0000

    Finished content discovery UI

--- src/main/java/org/t3/g11/proj2/peer/ui/swing/panels/DiscoveryPanel.java
+++ src/main/java/org/t3/g11/proj2/peer/ui/swing/panels/DiscoveryPanel.java
@@ -14,6 +14,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Set;
+import java.util.concurrent.Future;
 
 public class DiscoveryPanel extends JPanel {
 
@@ -22,7 +23,7 @@ public class DiscoveryPanel extends JPanel {
     private final DateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
     public DiscoveryPanel(SwingInterface swi) {
-        super(new MigLayout("inset 20, fill"));
+        super(new MigLayout("inset 20, fill, debug", "", "[][][nogrid][]"));
 
         this.swi = swi;
 
@@ -51,6 +52,15 @@ public class DiscoveryPanel extends JPanel {
         JTextField queryField = new JTextField();
         searchBox.add(queryField, "growx");
 
+        JPanel searched = new JPanel(new MigLayout("nogrid"));
+        searched.setVisible(false);
+        JLabel searchedForLabel = new JLabel("Query: ");
+        searchedForLabel.setEnabled(false);
+        searched.add(searchedForLabel, "");
+        JLabel currentQuery = new JLabel();
+        searched.add(currentQuery, "wrap");
+        add(searched, "wrap");
+
         this.postsPanel = new JPanel(new MigLayout("fillx"));
 
         JScrollPane scrollPane = new JScrollPane(postsPanel);
@@ -58,15 +68,18 @@ public class DiscoveryPanel extends JPanel {
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         scrollPane.getVerticalScrollBar().setUnitIncrement(16);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
-        add(scrollPane, "span, grow, pushy, wrap");
+        scrollPane.setVisible(false);
+        add(scrollPane, "span, grow, pushy, wrap, hidemode 3");
 
         // Search elements
         ImageIcon loadingIcon = swi.createImageIcon("loading.gif",
                 "loading");
-        JLabel loadingLabel = new JLabel(loadingIcon);
+        JLabel loadingLabel = new JLabel(loadingIcon, JLabel.CENTER);
+        loadingLabel.setVisible(false);
+        add(loadingLabel, "span, grow, pushy, align center, wrap, hidemode 3");
 
-        JLabel currentQuery = new JLabel();
-        currentQuery.setEnabled(false);
+        JLabel noResultsFound = new JLabel("", JLabel.CENTER);
+        add(noResultsFound, "span, grow, pushy, align center, wrap, hidemode 3");
 
         ActionListener querySubmit = new ActionListener() {
             @Override
@@ -75,32 +88,56 @@ public class DiscoveryPanel extends JPanel {
                 String q = queryField.getText().trim();
                 if (q.isBlank())
                     return;
+                queryField.setText("");
 
-                postsPanel.removeAll();
                 currentQuery.setText(q);
-                postsPanel.add(currentQuery, "wrap");
-                postsPanel.add(loadingLabel, "align center, wrap");
+                searched.setVisible(true);
+
+                noResultsFound.setText("");
+                noResultsFound.setVisible(false);
+
+                loadingLabel.setVisible(true);
+                scrollPane.setVisible(false);
+                postsPanel.removeAll();
                 invalidate();
                 repaint();
 
-                Set<Result> results = swi.peer.search(q);
+                Thread t = new Thread(() -> {
+                    Set<Result> results = swi.peer.search(q);
 
-                postsPanel.remove(loadingLabel);
+                    SwingUtilities.invokeLater(() -> {
+                        loadingLabel.setVisible(false);
 
-                // handle save query results
-                for (Result post : results) {
-                    System.out.println(post.author);
-                    String content;
-                    try {
-                        content = swi.peer.decypherText(post.ciphered, post.author);
-                        addNewPost(post.author, post.date, content);
-                    } catch (Exception exception) {
-                        System.err.println("Failed to add post with stacktrace:");
-                        exception.printStackTrace();
-                    }
-                }
+                        if (results.isEmpty()) {
+                            noResultsFound.setText("No results found");
+                            noResultsFound.setVisible(true);
+                        } else {
+                            scrollPane.setVisible(true);
+
+                            // handle save query results
+                            for (Result post : results) {
+                                System.out.println(post.author);
+                                String content;
+                                try {
+                                    content = swi.peer.decypherText(post.ciphered, post.author);
+                                    addNewPost(post.author, post.date, content);
+                                } catch (Exception exception) {
+                                    System.err.println("Failed to add post with stacktrace:");
+                                    exception.printStackTrace();
+                                }
+                            }
+                        }
+
+                        revalidate();
+                        repaint();
+                    });
+                });
+
+                t.start();
             }
         };
+
+
         searchButton.addActionListener(querySubmit);
         queryField.addActionListener(querySubmit);
     }
